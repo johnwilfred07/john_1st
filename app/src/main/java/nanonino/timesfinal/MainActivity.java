9e818f13788e8bc338fbed1e2c3f81ab1f1d7446
Gobinath <gobinath.b@nanonino.in>
2017-09-04 18:12:50 +0530

    3rd commit

--- app/src/main/java/nanonino/timesfinal/MainActivity.java
+++ app/src/main/java/nanonino/timesfinal/MainActivity.java
@@ -1,12 +1,12 @@
 package nanonino.timesfinal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import nanonino.mylibrary.Timesss;
+import nanonino.mylibrary.UTC_Converter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +18,10 @@ public class MainActivity extends AppCompatActivity {
         Calendar c = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss aa");
         String strDate = sdf.format(c.getTime());
-        System.out.println("the current time 1 is "+strDate);
-        Timesss timesss= new Timesss();
-       int time= timesss.getdifferencewith_two_times(strDate,strDate);
-        System.out.println("the time is  is "+time);
-
+        System.out.println("the current time 1 is " + strDate);
+        UTC_Converter utc_converter = new UTC_Converter();
+        int time = utc_converter.getdifferencewith_two_times(strDate, strDate);
+        System.out.println("the time is  is " + time);
 
 
     }
